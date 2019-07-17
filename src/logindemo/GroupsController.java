75a73c36355b14f3eb6fac9e6d24bd780ee75b89
nakhlarafi <16101197@UB71201PC16.bracu.ac.bd>
2019-07-17 17:27:38 +0600

    Groups added.

---
+++ src/logindemo/GroupsController.java
@@ -0,0 +1,22 @@
+package logindemo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author 16101197
+ */
+public class GroupsController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
